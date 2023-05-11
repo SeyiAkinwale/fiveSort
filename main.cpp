3768cd67621a66de9305dcac7db00b498227de8b
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-05-11 02:18:17 +0000

    rPointer lPointer

--- main.cpp
+++ main.cpp
@@ -4,17 +4,20 @@
 void swap(int &a, int &b)
 {int temp =a; a=b; b=temp; }
 
-std::vector<int>& fiveSort(std::vector<int> &numbers) {
-  // todo
+std::vector<int>& fiveSort(std::vector<int> &numbers) 
+{
+  int rPointer=numbers.size()-1;
+  for(int lPointer=0; lPointer<numbers.size()/2; lPointer++)
+  {
+    std::cout<<std::endl<<"hello";
+    
+  }
   return numbers;
 }
 
 int main()
 {
-  int val = 5, diffVal=10;
-  std::cout<<std::endl<<val<<diffVal;
-
-  swap(val,diffVal);
-  std::cout<<std::endl<<val<<diffVal;
+  std::vector<int> numbers {12, 5, 1, 5, 12, 7};
+  fiveSort(numbers);
   return 0;
 }
