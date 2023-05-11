58732dbb8bdd991bdc468c68d4f99d1304e3340a
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-05-11 02:44:43 +0000

    lPointer<rPointer

--- .ccls-cache/@home@runner@fiveSort/main.cpp
+++ .ccls-cache/@home@runner@fiveSort/main.cpp
@@ -7,10 +7,17 @@ void swap(int &a, int &b)
 std::vector<int>& fiveSort(std::vector<int> &numbers) 
 {
   int rPointer=numbers.size()-1;
-  for(int lPointer=0; lPointer<numbers.size()/2; lPointer++)
+  for(int lPointer=0; lPointer<rPointer; lPointer++)
   {
     std::cout<<std::endl<<"hello";
-    
+
+    if(numbers[lPointer]==5  &&  numbers[rPointer]==5)
+    {lPointer--; rPointer--;}
+    else if(numbers[lPointer]==5  && numbers[rPointer]!=5)
+    {
+      swap(numbers[lPointer], numbers[rPointer]);
+      rPointer--;
+    }    
   }
   return numbers;
 }
@@ -19,5 +26,8 @@ int main()
 {
   std::vector<int> numbers {12, 5, 1, 5, 12, 7};
   fiveSort(numbers);
+
+  for(int i=0; i<numbers.size(); i++) 
+    std::cout<<numbers[i]<<" ";
   return 0;
 }
