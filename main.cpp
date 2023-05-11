948a1dab8b75eeec0a00b2768b647a8ccb6883a5
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-05-11 02:05:00 +0000

    swap func

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <vector>
 
+void swap(int &a, int &b)
+{int temp =a; a=b; b=temp; }
+
 std::vector<int>& fiveSort(std::vector<int> &numbers) {
   // todo
-  return result;
+  return numbers;
 }
 
 int main()
 {
+  std::cout<<val<<endl<<diffVal;
+  int val = 5, diffVal=10;
+  swap(val,diffVal);
+  std::cout<<val<<endl<<diffVal;
   return 0;
 }
